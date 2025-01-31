3f2c9a466feff9c00a9c966a5a7305e1fd88de72
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-01-31 19:01:52 +0530

    commit

--- djangoframesorg/devs/devs/settings.py
+++ djangoframesorg/devs/devs/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'devs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'products',
+        'USER':'root',
+        'PASSWORD':'6463',
+        
+        
+        'HOST':'localhost',
+        'PORT':3306
+        
     }
 }
 
