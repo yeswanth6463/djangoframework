13da2707a03a12656c391ef3016ea3d716e0467b
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-02-05 08:25:05 +0530

    forms validation

--- djangoframesorg/empforms/empfapp/forms.py
+++ djangoframesorg/empforms/empfapp/forms.py
@@ -1,8 +1,44 @@
-from django import forms
+from django import forms 
 
 class EmployeeForm(forms.Form):
-    name=forms.CharField(max_length=100)
-    age=forms.IntegerField()
-    phone=forms.IntegerField()
-    address=forms.CharField(max_length=250)
+    name = forms.CharField(max_length=100)
+    age  = forms.IntegerField()
+    phone  = forms.IntegerField()
+    password = forms.CharField(max_length=100,widget=forms.PasswordInput)
+    confirm_password = forms.CharField(max_length=100,widget=forms.PasswordInput)
+    address = forms.CharField(max_length=250)
     
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if ord(name[0]) >= 45 and ord(name[0]) <=57:
+            raise forms.ValidationError('Name should not startswith numbers')
+        return name
+
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        if age <= 0:
+            raise forms.ValidationError('Age must be positive value and above 0')
+        return age
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if len(str(phone)) !=10:
+            raise forms.ValidationError('phone number should contains 10 digits ')
+        return phone
+
+    # def clean_confirm_password(self):
+    #     password = self.cleaned_data['password']
+    #     confirm_password = self.cleaned_data['confirm_password']
+    #     if password !=  confirm_password:
+    #         raise forms.ValidationError('This password is unmatching')
+    #     return confirm_password
+
+    def clean(self):
+        cleaned_data = super().clean()
+        
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password !=  confirm_password:
+            raise forms.ValidationError('This password is unmatching')
+        return confirm_password
--- djangoframesorg/empforms/empfapp/views.py
+++ djangoframesorg/empforms/empfapp/views.py
@@ -3,6 +3,17 @@ from empfapp.forms import EmployeeForm
 
 # Create your views here.
 def index(request):
-    form=EmployeeForm()
-    return render(request,'index.html',{'form':form})
+    form = EmployeeForm()
 
+    if request.method =='POST':
+        form = EmployeeForm(request.POST)
+
+        if form.is_valid():
+            print('validation success')
+
+            print(form.cleaned_data['name'])
+            print(form.cleaned_data['age'])
+            print(form.cleaned_data['phone'])
+            print(form.cleaned_data['address'])
+
+    return render(request,'index.html',{'form':form})
