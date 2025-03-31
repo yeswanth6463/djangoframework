56a66e92bc02ae75d6e41041919877359d4f7b98
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-03-31 18:47:15 +0530

    commit

---
+++ shopkart/shop/form.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class CustomerForm(UserCreationForm):
+    class Meta:
+        
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2',
+            
+        ]
--- shopkart/shop/views.py
+++ shopkart/shop/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render ,redirect ,HttpResponse
 from . models import Category,Product
+from .form import CustomerForm
 from django.contrib import messages
 # Create your views here.
 def home(request):
-    return render(request, 'shop/index.html')
+    product=Product.objects.filter(trending=True)
+    return render(request,'shop/index.html',{'product':product})
+    
+    
 
 def register(request):
-    return render(request, 'shop/register.html')
+    form=CustomerForm()
+    return render(request, 'shop/register.html', {'form':form})
 
 def collections(request):
     catagory=Category.objects.filter(status=0)
@@ -20,5 +25,14 @@ def collectionsView(request,name):
         messages.warning(request,"No such Catagory Found")
         return redirect('collections')
 
-def product_details(request,cnmae,pname):
-    return HttpResponse("product details")
+def product_details(request,cname,pname):
+   if(Category.objects.filter(name=cname,status=0)):
+       if(Product.objects.filter(name=pname,category__name=cname)):
+           product=Product.objects.get(name=pname,category__name=cname)
+           return render(request, 'shop/products/product_details.html',{'product':product})
+       else:
+           messages.warning(request,"No such Product Found")
+           return redirect('collections')
+   else:
+       messages.warning(request,"No such Catagory Found")
+       return redirect("collections")
