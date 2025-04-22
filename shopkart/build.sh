b583a0191320efba801f0de1378916b0da9b7a72
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-04-22 11:15:01 +0530

    confirnm

--- shopkart/build.sh
+++ shopkart/build.sh
@@ -1,11 +1,7 @@
-set -o errexit
+#!/bin/bash
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-
-if [[$CREATE_SUPERUSER]];
-then
-python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL" fi
+# Run the Django app with gunicorn
+gunicorn shopkart.wsgi:application --bind 0.0.0.0:8000
