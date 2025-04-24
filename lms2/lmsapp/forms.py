309eb02500c35a60bf6188d3c11fd7f57b685ded
YESWANTH <113580288+yeswanth6463@users.noreply.github.com>
2025-04-24 17:39:31 +0530

    n'

--- lms2/lmsapp/forms.py
+++ lms2/lmsapp/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Student
 
-
-
-
 class StudentForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
